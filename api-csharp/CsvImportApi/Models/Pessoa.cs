1379a30aa6dd36a09a5b74bfb41da90290f03ce9
joaowozniack <90704865+joaowozniack@users.noreply.github.com>
2025-02-13 15:56:57 -0300

    Criacao do Context AppDbContext e Models Pessoa

---
+++ api-csharp/CsvImportApi/Models/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CsvImportApi.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public int Idade { get; set; }
+        public string Cidade { get; set; } = string.Empty;
+        public string Profissao { get; set; } = string.Empty;
+    }
+}
