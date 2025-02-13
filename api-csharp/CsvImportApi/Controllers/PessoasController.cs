b163f5ca5f81775c41e30892cf98141ae443ec5f
joaowozniack <90704865+joaowozniack@users.noreply.github.com>
2025-02-13 18:10:37 -0300

    Criacao da controller com os endpoint de importar e buscar registro, criacao do csv

---
+++ api-csharp/CsvImportApi/Controllers/PessoasController.cs
@@ -0,0 +1,68 @@
+﻿using CsvImportApi.Context;
+using CsvImportApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CsvImportApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PessoasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public PessoasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //Endpoint: imporar CSV
+        [HttpPost("importar")]
+        public async Task<IActionResult> ImportarCsv(IFormFile arquivo)
+        {
+            if (arquivo == null || arquivo.Length == 0)
+            {
+                return BadRequest("Arquivo CSV inválido");
+            }
+
+            using var leitor = new StreamReader(arquivo.OpenReadStream());
+            await leitor.ReadLineAsync(); // Ignora a primeira linha
+
+            while (!leitor.EndOfStream)
+            {
+                var linha = await leitor.ReadLineAsync();
+                if (linha == null)
+                {
+                    continue;
+                }
+
+                var valores = linha.Split(';');
+
+                var pessoa = new Pessoa
+                {
+                    Nome = valores[1],
+                    Idade = int.Parse(valores[2]),
+                    Cidade = valores[3],
+                    Profissao = valores[4]
+                };
+
+                _context.Pessoas.Add(pessoa);
+            }
+
+            await _context.SaveChangesAsync();
+            return Ok("O arquivo foi importado com sucesso");
+        }
+
+        //Endpoint: buscar registro por ID e retornar JSON
+        [HttpGet("{id}")]
+        public async Task<IActionResult> BuscarRegistro(int id)
+        {
+            var pessoa = await _context.Pessoas.FindAsync(id);
+            if (pessoa == null)
+            {
+                return NotFound("Registro não encontrado.");
+            }
+
+            return Ok(pessoa);
+        }
+    }
+}
